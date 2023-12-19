7159f4addc614970cbb5a2424d37165bef29bac9
Theo Vambre <theo.vambre@ens.univ-artois.fr>
2023-12-19 09:51:07 +0100

    Creation du header close #27 :memo:

---
+++ poc-sae3-01-grp02/resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<nav>
+    <div class="gauche">
+        <a href="{{route('accueil')}}">Accueil</a>
+        <a href="{{route('apropos')}}">a Propos</a>
+        <a href="">Contacts</a>
+        <a href="">Listes Scènes</a>
+    </div>
+    <div class="centre">
+        <h1>PornoHerrmann</h1>
+    </div>
+    <div class="droite">
+        <a href="">Connexion</a>
+        <a href="">Creer un compte</a>
+    </div>
+</nav>
+
--- poc-sae3-01-grp02/routes/web.php
+++ poc-sae3-01-grp02/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('accueil');
+})->name('accueil');
+
+Route::get('/apropos', function (){
+    return view('apropos');
+})->name('apropos');
