98837ed2ce35ec4e66ed006a0e70f0a187c53eaf
Theo Vambre <theo.vambre@ens.univ-artois.fr>
2023-12-19 17:25:37 +0100

    Modification de la page a propos

--- poc-sae3-01-grp02/resources/views/apropos.blade.php
+++ poc-sae3-01-grp02/resources/views/apropos.blade.php
@@ -5,14 +5,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/accueil.css', 'resources/css/footer.css'])
+    @vite(['resources/css/accueil.css', 'resources/css/apropos.css', 'resources/css/footer.css'])
 </head>
 <body>
 <header>
     <x-header></x-header>
 </header>
-<p>Voici notre l'explication de notre site sur les images du Raytracing</p>
-<p></p>
+<div class="sec1">
+    <h1>Description de notre musée</h1>
+    <p>Nous vous présentons notre site internet du musée Raytracing avec plusieurs images avec leur nom, descriptif,
+        des commentaires le nom de l'auteur, la date d'ajoute de l'images, et le lien de la vignette de cette image.
+</div>
+<div class="sec2">
+    <h1>Notre Equipe</h1>
+    <div class="contenu">
+        <p>Louis Dutombois</p>
+        <p>Chef de projet</p>
+        <img src="">
+    </div>
+    <div class="contenu">
+        <p>Alex Pochet</p>
+        <p>Développeur</p>
+        <img src="">
+    </div>
+    <div class="contenu">
+        <p>Hugo Dubuisson</p>
+        <p>Développeur</p>
+        <img src="">
+    </div>
+    <div class="contenu">
+        <p>Théo Vambre</p>
+        <p>Développeur</p>
+        <img src="">
+    </div>
+</div>
+<div class="sec3">
+    <h1>Nos collaborateurs</h1>
+    <p>
+        ok
+    </p>
+</div>
+<div class="sec4">
+    <h1>Nos engagements</h1>
+    <p>
+        ok
+    </p>
+</div>
+
 <footer>
     <x-footer></x-footer>
 </footer>
